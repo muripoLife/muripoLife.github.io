a219e74371496868fe2de82645c1a4ee5e95a2f8
muripoLife <life.is.too.high.difficulty@gmail.com>
2017-04-04 01:24:42 +0900

    plusMath

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 使っているライブラリ
 * React.js
 * three.js
+* MathJax.js
 
 # [ページ](https://muripolife.github.io/mypage_home.html)
--- motion_01.js
+++ motion_01.js
@@ -1,21 +1,15 @@
 const scene = new THREE.Scene();
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xcccccc);
 renderer.setPixelRatio(window.devicePixelRatio);
-
 document.body.appendChild(renderer.domElement);
 
 // 遠近効果があるカメラ
 const camera        = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 camera.position.z   = 30;
-
 const controls      = new THREE.OrbitControls(camera);
-
 const target        = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
-
 const shaderScene   = new THREE.Scene();
 // 遠近効果がないカメラ
 const shaderCamera  = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -1, 1);
@@ -40,13 +34,10 @@ const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial);
 
 shaderScene.add(shaderMesh);
 
-const geometry = new THREE.CubeGeometry(20, 20, 20);
+const geometry = new THREE.CubeGeometry(30, 25, 1);
 const material = new THREE.MeshBasicMaterial();
-
 const mesh     = new THREE.Mesh(geometry, material);
-
 scene.add(mesh);
-
 const clock    = new THREE.Clock();
 
 (function animate(){
