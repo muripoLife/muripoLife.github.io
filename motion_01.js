e9981d861e37758d74befd773c6b0ced89aba69e
muripoLife <life.is.too.high.difficulty@gmail.com>
2017-04-21 00:47:38 +0900

    move_char

--- motion_01.js
+++ motion_01.js
@@ -45,71 +45,74 @@ const clock    = new THREE.Clock();
 const light = new THREE.HemisphereLight(0xff99ad, 0xfef4f4);
 light.position.y = 1050;
 
+let count = 0;
+
 scene.add(light);
 var loader = new THREE.FontLoader();
-loader.load( 'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function ( font ) {
-	var textGeo_bf = new THREE.TextGeometry( "x^2+y^2+z^2=r^2", {
-		font: font,
-		size: 30,
-		height: 10,
-		curveSegments: 12,
-		bevelThickness: 1,
-		bevelSize: 1,
-		bevelEnabled: true
-	});
-
-	var textGeo_arrow = new THREE.TextGeometry( "----------------------", {
-		font: font,
-		size: 50,
-		height: 10,
-		curveSegments: 12,
-		bevelThickness: 1,
-		bevelSize: 1,
-		bevelEnabled: true
-	});
-
-
-	var textGeo_af = new THREE.TextGeometry( "x^2+y^2+z^2+R^2-2R(x2+y2)^1/2=r^2", {
-		font: font,
-		size: 18,
-		height: 5,
-		curveSegments: 2,
-		bevelThickness: 1,
-		bevelSize: 1,
-		bevelEnabled: true
-	});
-
-	textGeo_bf.computeBoundingBox();
-	textGeo_af.computeBoundingBox();
-	textGeo_arrow.computeBoundingBox();
-	var textMaterial = new THREE.MeshPhongMaterial( { color: 0x00ffff, specular: 0xffffff } );
-	var meshText_bf = new THREE.Mesh( textGeo_bf, textMaterial );
-	var meshText_af = new THREE.Mesh( textGeo_af, textMaterial );
-	var meshText_arrow = new THREE.Mesh( textGeo_arrow, textMaterial );
-	meshText_bf.position.x = -400;
-	meshText_bf.position.y = 60;
-	meshText_bf.position.z = -400;
-	meshText_bf.castShadow = true;
-	meshText_bf.receiveShadow = true;
-
-	meshText_arrow.position.x = -500;
-	meshText_arrow.position.y = 0;
-	meshText_arrow.position.z = -400;
-	meshText_arrow.castShadow = true;
-	meshText_arrow.receiveShadow = true;
-
-	meshText_af.position.x = -450;
-	meshText_af.position.y = -60;
-	meshText_af.position.z = -400;
-	meshText_af.castShadow = true;
-	meshText_af.receiveShadow = true;
-	scene.add( meshText_bf, meshText_af, meshText_arrow);
-});
 
 (function animate(){
 	requestAnimationFrame(animate);
 	renderer.render(shaderScene, shaderCamera, target);
 	material.map = target.texture;
 	shaderUniforms.time.value = clock.getElapsedTime();
+
+	loader.load( 'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function ( font ) {
+		var textGeo_bf = new THREE.TextGeometry( "x^2+y^2+z^2=r^2", {
+			font: font,
+			size: 30,
+			height: 10,
+			curveSegments: 12,
+			bevelThickness: 1,
+			bevelSize: 1,
+			bevelEnabled: true
+		});
+
+		var textGeo_arrow = new THREE.TextGeometry( "----------------------", {
+			font: font,
+			size: 50,
+			height: 10,
+			curveSegments: 12,
+			bevelThickness: 1,
+			bevelSize: 1,
+			bevelEnabled: true
+		});
+
+		var textGeo_af = new THREE.TextGeometry( "x^2+y^2+z^2+R^2-2R(x2+y2)^1/2=r^2", {
+			font: font,
+			size: 18,
+			height: 5,
+			curveSegments: 2,
+			bevelThickness: 1,
+			bevelSize: 1,
+			bevelEnabled: true
+		});
+
+		textGeo_bf.computeBoundingBox();
+		textGeo_af.computeBoundingBox();
+		textGeo_arrow.computeBoundingBox();
+		var textMaterial = new THREE.MeshPhongMaterial( { color: 0x00ffff, specular: 0xffffff } );
+		var meshText_bf = new THREE.Mesh( textGeo_bf, textMaterial );
+		var meshText_af = new THREE.Mesh( textGeo_af, textMaterial );
+		var meshText_arrow = new THREE.Mesh( textGeo_arrow, textMaterial );
+		meshText_bf.position.x = -400;
+		meshText_bf.position.y = 60*Math.sin(count * 0.005);
+		meshText_bf.position.z = -400;
+		meshText_bf.castShadow = true;
+		meshText_bf.receiveShadow = true;
+
+		meshText_arrow.position.x = -500;
+		meshText_arrow.position.y = 0;
+		meshText_arrow.position.z = -400;
+		meshText_arrow.castShadow = true;
+		meshText_arrow.receiveShadow = true;
+
+		meshText_af.position.x = -450;
+		meshText_af.position.y = -60;
+		meshText_af.position.z = -400;
+		meshText_af.castShadow = true;
+		meshText_af.receiveShadow = true;
+		scene.add( meshText_bf, meshText_af, meshText_arrow);
+	});
+	count++;
 	renderer.render(scene, camera);
 }());
