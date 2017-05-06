cd99e5a5080fe68fe3a25127174158cf2ae33f22
muripoLife <life.is.too.high.difficulty@gmail.com>
2017-05-06 15:16:03 +0900

    complete regularTetrahedron

--- regularTetrahedron.js
+++ regularTetrahedron.js
@@ -47,33 +47,33 @@
 		let count = 0;
 
 		scene.add(light);
-		var loader = new THREE.FontLoader();
+		// var loader = new THREE.FontLoader();
 
-		var geoText = function ( font ) {
-			var textGeo_bf = new THREE.TextGeometry( "RegularTetrahedron", {
-				font: font,
-				size: 30,
-				height: 10,
-				curveSegments: 12,
-				bevelThickness: 1,
-				bevelSize: 1,
-				bevelEnabled: true
-			});
+		// var geoText = function ( font ) {
+		// 	var textGeo_bf = new THREE.TextGeometry( "RegularTetrahedron", {
+		// 		font: font,
+		// 		size: 30,
+		// 		height: 10,
+		// 		curveSegments: 12,
+		// 		bevelThickness: 1,
+		// 		bevelSize: 1,
+		// 		bevelEnabled: true
+		// 	});
 		
-			textGeo_bf.computeBoundingBox();
-			var textMaterial = new THREE.MeshPhongMaterial( { color: 0x00ffff, specular: 0xffffff } );
-			var meshText_bf = new THREE.Mesh( textGeo_bf, textMaterial );
-			meshText_bf.castShadow = true;
-			meshText_bf.receiveShadow = true;
-			return meshText_bf;
-		};
+		// 	textGeo_bf.computeBoundingBox();
+		// 	var textMaterial = new THREE.MeshPhongMaterial( { color: 0x00ffff, specular: 0xffffff } );
+		// 	var meshText_bf = new THREE.Mesh( textGeo_bf, textMaterial );
+		// 	meshText_bf.castShadow = true;
+		// 	meshText_bf.receiveShadow = true;
+		// 	return meshText_bf;
+		// };
 
-		const font = await loadFont('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json');
-		const textMesh = geoText(font);
-		textMesh.scale.set(0.05, 0.05, 0.05);
-		textMesh.position.set(-25, 7, 0);
+		// const font = await loadFont('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json');
+		// const textMesh = geoText(font);
+		// textMesh.scale.set(0.05, 0.05, 0.05);
+		// textMesh.position.set(-25, 7, 0);
 
-		scene.add(textMesh);
+		// scene.add(textMesh);
 
 		animate();
 
